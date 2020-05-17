4d3cd741f2a4e9b434582f5b909a8ba27c2923be
sourabhvishnav <62550282+sourabhvishnav@users.noreply.github.com>
2020-05-17 10:07:41 +0530

    17/05/20
    shivang bairagi

---
+++ box.js
@@ -0,0 +1,27 @@
+class Box{
+    constructor(x, y, width, height) {
+        var options = {
+            'restitution':0.1,
+            'friction':0.1,
+            'density':1.2
+        }
+  this.body = Bodies.rectangle(x,y,width,height,options);
+  this.width = width;
+  this.height = height;
+  this.color ="white"; 
+  this.Visiblity = 255;   
+  World.add(world,this.body);
+}
+ display(){
+     if(this.body.speed<5){
+     var pos = this.body.position;
+     rectMode(CENTER);
+     fill(this.color);
+     rect(pos.x,pos.y,this.width,this.height);
+     }
+     else{
+        World.remove(world, this.body);
+        this.Visiblity = this.Visiblity - 5;        
+  }
+      }
+  }
